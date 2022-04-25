cd485d86d4583b232266a747d02d2c641b1dfa10
Карина Миронова <Twiksyyy@gmail.com>
2022-04-25 15:00:25 +0300

    added other files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int bonusMiles = 20;
+        System.out.println("Вам начисленно бонусных миль: " + ticketPrice / bonusMiles);
+
+
+    }
+}
